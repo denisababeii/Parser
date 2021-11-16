d90292753a2dbfacd8539795fe53cd0c9f432119
Denisa Babeii <babeii.denisa@yahoo.ro>
2021-11-16 13:44:58 +0200

    Add Grammar and Production empty classes

---
+++ src/main/java/Grammar.java
@@ -0,0 +1,32 @@
+import java.util.List;
+
+public class Grammar {
+    private boolean isCFG;
+    private List<String> terminals;
+    private List<String> nonTerminals;
+    private List<Production> productions;
+
+    public void read(String file) {
+
+    }
+
+    public void printNonTerminals() {
+
+    }
+
+    public void printTerminals() {
+
+    }
+
+    public void printProductions() {
+
+    }
+
+    public void printProductionsForNonTerminal(String nonTerminal) {
+
+    }
+
+    public void checkCFG() {
+
+    }
+}
