619d27f8fa6dac44612c4d565bdbc6f8233b6b46
Denisa Babeii <babeii.denisa@yahoo.ro>
2021-11-16 14:33:13 +0200

    Add read, printTerminals, printNonTerminals

--- src/main/java/Grammar.java
+++ src/main/java/Grammar.java
@@ -1,4 +1,6 @@
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Grammar {
@@ -6,6 +8,7 @@ public class Grammar {
     private List<String> terminals;
     private List<String> nonTerminals;
     private List<Production> productions;
+    private String startingSymbol;
 
     public Grammar(String file) {
         terminals = new ArrayList<>();
@@ -16,15 +19,61 @@ public class Grammar {
     }
 
     public void read(String file) {
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String line = reader.readLine();
+            int lineCount = 1;
+            while (line != null) {
+                if (lineCount == 1) { // if I am on the first line, where the terminals are enumerated
+                    var terminals = line.split(" ");
+                    this.terminals.addAll(Arrays.asList(terminals));
+                } else if (lineCount == 2) { // if I am on the second line, where the nonTerminals are enumerated
+                    var nonTerminals = line.split(" ");
+                    this.nonTerminals.addAll(Arrays.asList(nonTerminals));
+                } else if (lineCount == 3) { // if I am on the third line, where the starting symbol is
+                    startingSymbol = line;
+                } else {
+                    var sides = line.split("->");
+                    var leftHandSide = sides[0];
+                    var rightHandSide = sides[1];
 
+                    List<List<String>> rules = new ArrayList<>();
+                    List<String> symbols = new ArrayList<>();
+                    for (String rule: rightHandSide.split("\\|"))
+                        rules.add(Arrays.asList(rule.split(" ")));
+                    for (String symbol: leftHandSide.split(" "))
+                        symbols.add(symbol);
+                    if(symbols.size()>1) // if there isn't a single symbol on the left-hand side
+                        isCFG = false;
+                    productions.add(new Production(symbols, rules));
+                }
+                line=reader.readLine();
+                lineCount++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void printNonTerminals() {
-
+        var result = new StringBuilder();
+        for (var nonTerminal:
+             nonTerminals) {
+            result.append(nonTerminal+", ");
+        }
+        result.delete(result.length()-2, result.length());
+        System.out.println(result.toString());
     }
 
     public void printTerminals() {
-
+        var result = new StringBuilder();
+        for (var terminal:
+                terminals) {
+            result.append(terminal+", ");
+        }
+        result.delete(result.length()-2, result.length());
+        System.out.println(result.toString());
     }
 
     public void printProductions() {
